9a62355b48cfef90eeb55618253b86f278aaa75a
wujuhui <756963862@qq.com>
2019-01-06 09:52:23 +0800

    增加了省级地图

--- public/javascripts/china_map.js
+++ public/javascripts/china_map.js
@@ -1,15 +1,14 @@
 function drawMap(mapPath, svg, c) {
     var projection = d3.geo.mercator()
         .center([108, 38])   //宽度，高度
-        .scale(width / 2 - 40)
-        .translate([width / 4 + 80, height / 2])
+        .scale(width / 2 - 200)
+        .translate([width / 4 - 30, height / 2])
     var path = d3.geo.path().projection(projection);
     // var nodes = [];
     d3.json(mapPath, function (error, root) {
         var backColor;
         if (error)
             return console.error(error)
-        console.log(root.features)
         svg.selectAll(".pathChina")
             .data(root.features)
             .enter()
@@ -25,13 +24,14 @@ function drawMap(mapPath, svg, c) {
 
             .on("mouseover", function (d, i) {
                 backColor = d3.select(this).attr("fill");
-               var colorPre = d3.select(this).attr("fill", overColor)
+                var colorPre = d3.select(this).attr("fill", overColor)
             })
             .on("mouseout", function (d, i) {
-                d3.select(this).attr("fill", backColor).on("click", function (d, i) {
-                    var id = d.properties.id;
-                    //  clickChina(d, i, "mapdata/geometryProvince/" + id + ".json")
-                })
+                d3.select(this).attr("fill", backColor)
+                    .on("click", function (d, i) {
+                        var id = d.properties.id;
+                        clickChina(d, i, "../json/geometryProvince/" + id + ".json")
+                    })
             })
         root.features.forEach(function (d, i) {
             var centroid = path.centroid(d)
@@ -47,3 +47,49 @@ function drawMap(mapPath, svg, c) {
 // d3.json("../json/data/year.json",function (error,root) {
 
 //   })
+
+function clickChina(d, i, path) {
+    d3.selectAll(".pathProvince").remove();
+    d3.selectAll(".pathCouty").remove();
+    drawPrivenceMap(path,d, svg)
+}
+function drawPrivenceMap(mapPath,d, svg) {
+    d3.json(mapPath, function (error, root) {
+
+        if(error)
+         return console.error(error)
+         console.log(root.features);
+        var projection = d3.geo.mercator() //投影函数
+            .center([128, 45])
+            .scale(850)
+            .translate([width / 4 * 3, height / 2]);
+        var path = d3.geo.path().projection(projection)
+        svg.selectAll(".pathProvince")
+            .data(root.features)
+            .enter()
+            .append("path")
+            .attr("class", "pathProvince")
+            .attr("stroke", "#000")
+            .attr("stroke-width", 0.3)
+            .attr("fill", function (d, i) {
+                return background;
+            })
+            .attr("d", path)
+            .on("mouseover", function (d, i) {
+                d3.select(this).attr("fill", overColor);
+            })
+            .on("mouseout", function (d, i) {
+                d3.select(this)
+                    .attr("fill", background);
+            })
+
+        root.features.forEach(function (d, i) {
+            var centroid = path.centroid(d)
+            centroid.x = centroid[0];
+            centroid.y = centroid[1];
+            centroid.id = d.properties.name
+            centroid.feature = d
+            proveinceNodes.push(centroid); ////这里不明白
+        })
+    });
+}
--- public/javascripts/privence_map.js
+++ public/javascripts/privence_map.js
@@ -1,34 +1,34 @@
-function drawPrivenceMap(mapPath,svg,c){
-    var projection = d3.geo.mercator() //投影函数
-                       .center(root.cp)
-                       .scale(root.size*2.7)
-                       .translate([width/4*3,height/2]);
-    var path = d3.geo.path().projection(projection)
-    svg.selectAll(".pathProvince")
-       .data(root.features)
-       .enter()
-       .append("path")
-       .attr("class","pathProvince")
-       .attr("stroke","#000")
-       .attr("stroke-width",0.3)
-       .attr("fill",function (d,i) {
-           return background;
-         })
-        .attr("d",path)
-        .on("mouseover",function (d,i) {
-         d3.select(this).attr("fill",overColor);
-         })
-        .on("mouseout",function (d,i) {
-           d3.select(this)
-              .attr("fill",background);
-          })
+// function drawPrivenceMap(mapPath,svg,c){
+//     var projection = d3.geo.mercator() //投影函数
+//                        .center(root.cp)
+//                        .scale(root.size*2.7)
+//                        .translate([width/4*3,height/2]);
+//     var path = d3.geo.path().projection(projection)
+//     svg.selectAll(".pathProvince")
+//        .data(root.features)
+//        .enter()
+//        .append("path")
+//        .attr("class","pathProvince")
+//        .attr("stroke","#000")
+//        .attr("stroke-width",0.3)
+//        .attr("fill",function (d,i) {
+//            return background;
+//          })
+//         .attr("d",path)
+//         .on("mouseover",function (d,i) {
+//          d3.select(this).attr("fill",overColor);
+//          })
+//         .on("mouseout",function (d,i) {
+//            d3.select(this)
+//               .attr("fill",background);
+//           })
 
-        root.features.forEach(function(d,i){
-              var centroid = path.centroid(d)
-            centroid.x = centroid[0];
-            centroid.y = centroid[1];
-            centroid.id = d.properties.name
-            centroid.feature = d
-            proveinceNodes.push(centroid); ////这里不明白
-        })
-}
+//         root.features.forEach(function(d,i){
+//               var centroid = path.centroid(d)
+//             centroid.x = centroid[0];
+//             centroid.y = centroid[1];
+//             centroid.id = d.properties.name
+//             centroid.feature = d
+//             proveinceNodes.push(centroid); ////这里不明白
+//         })
+// }
