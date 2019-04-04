d9a71ef210da1e01ea68e062bf9fac8d9453a834
wujuhui <756963862@qq.com>
2019-04-04 11:34:46 +0800

    完成了老年人口图谱

---
+++ public/javascripts/house1.js
@@ -0,0 +1,91 @@
+function house1(uid) {
+    var house1Chart = echarts.init(document.getElementById("main-bottom-left"), "dark");
+    house1Chart.showLoading();
+    d3.csv("../csv/house1.csv", function (error, dataset) {
+        var i = 0;
+        var id = uid || 0;
+        if (error)
+            console.error(error)
+        console.log(dataset);
+        option = {
+            title: {
+                text: '职业与住房',
+                x: '0',
+                y:'5%'
+            },
+            tooltip : {
+                trigger: 'axis',
+                axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                    type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                }
+            },
+            angleAxis: {
+                type: 'category',
+                data: ['公务员', '技术员', '办事员', '商人', '农民', '工人', '其他'],
+                z: 10          
+            },
+            radiusAxis: {
+            },
+            polar: {
+            },
+            series: [  {
+                type: 'bar',
+                data: [31.40,38.73	,35.26	, 34.92	, 31.22	, 30.85, 30.24	,30.66	],
+                coordinateSystem: 'polar',
+                name: '人均住房面积',
+                stack: 'a',
+             
+            }, {
+                type: 'bar',
+                data: [1.01,1.02, 1.01,0.97, 0.95, 1.07, 0.94,0.96],
+                coordinateSystem: 'polar',
+                name: '人均住房间数',
+                stack: 'a',
+                itemStyle: {   
+                    //通常情况下：
+                    normal:{  
+    　　　　　　　　　　　　//每个柱子的颜色即为colorList数组里的每一项，如果柱子数目多于colorList的长度，则柱子颜色循环使用该数组
+                        color: ['rgb(42,170,227)']
+                            
+                    },
+                    //鼠标悬停时：
+                    emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                },
+            }, {
+                type: 'bar',
+                data: [3.29	,3.10	, 2.75	,2.73, 2.73, 3.80, 2.92,2.96],
+                coordinateSystem: 'polar',
+                name: '平均每户住房间数',
+                stack: 'a',
+                itemStyle: {   
+                    //通常情况下：
+                    normal:{  
+    　　　　　　　　　　　　//每个柱子的颜色即为colorList数组里的每一项，如果柱子数目多于colorList的长度，则柱子颜色循环使用该数组
+                        color: ['green']
+                            
+                    },
+                    //鼠标悬停时：
+                    emphasis: {
+                            shadowBlur: 10,
+                            shadowOffsetX: 0,
+                            shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                },
+            }],
+            legend: {
+                show: true,
+                data: ['人均住房面积', '人均住房间数', '平均每户住房间数'],
+                x:'75%'
+            }
+        };
+        
+            
+        house1Chart.setOption(option);
+        house1Chart.hideLoading();
+    });
+}
+house1();
---
+++ public/javascripts/house2.js
@@ -0,0 +1,151 @@
+function house2(uid){
+var house2Chart = echarts.init(document.getElementById("main-top-right"),"dark");
+house2Chart.showLoading();
+d3.csv("../csv/house2.csv", function (error, dataset) {
+    if (error)
+        console.error(error)
+    console.log(dataset);  //人口数据
+
+     house2Chart.hideLoading();
+     var id = uid ||0;
+
+     var hushu=[];
+     var humianji=[];
+     var jianmianji=[];
+     hushu.push(dataset[id].hushu1949);
+     hushu.push(dataset[id].hushu1959);
+     hushu.push(dataset[id].hushu1969);
+     hushu.push(dataset[id].hushu1979);
+     hushu.push(dataset[id].hushu1989);
+     hushu.push(dataset[id].hushu1999);
+     hushu.push(dataset[id].hushu2000);
+     console.log(hushu);
+
+     humianji.push((dataset[id].mianji1949/dataset[id].hushu1949).toFixed(2));
+     humianji.push((dataset[id].mianji1959/dataset[id].hushu1959).toFixed(2));
+     humianji.push((dataset[id].mianji1969/dataset[id].hushu1969).toFixed(2));
+     humianji.push((dataset[id].mianji1979/dataset[id].hushu1979).toFixed(2));
+     humianji.push((dataset[id].mianji1989/dataset[id].hushu1989).toFixed(2));
+     humianji.push((dataset[id].mianji1999/dataset[id].hushu1999).toFixed(2));
+     humianji.push((dataset[id].mianji2000/dataset[id].hushu2000).toFixed(2));
+     console.log(humianji)
+
+    jianmianji.push((dataset[id].mianji1949/dataset[id].jianshu1949).toFixed(2));
+    jianmianji.push((dataset[id].mianji1959/dataset[id].jianshu1959).toFixed(2));
+    jianmianji.push((dataset[id].mianji1969/dataset[id].jianshu1969).toFixed(2));
+    jianmianji.push((dataset[id].mianji1979/dataset[id].jianshu1979).toFixed(2));
+    jianmianji.push((dataset[id].mianji1989/dataset[id].jianshu1989).toFixed(2));
+    jianmianji.push((dataset[id].mianji1999/dataset[id].jianshu1999).toFixed(2));
+    jianmianji.push((dataset[id].mianji2000/dataset[id].jianshu2000).toFixed(2));
+    console.log(jianmianji);
+
+        var colors = ['#5793f3', '#d14a61', '#675bba'];
+        
+        option = {
+            color: colors,
+        
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'cross'
+                }
+            },
+            grid: {
+                right: '20%'
+            },
+            toolbox: {
+                feature: {
+                    dataView: {show: true, readOnly: false},
+                    restore: {show: true},
+                    saveAsImage: {show: true}
+                }
+            },
+            legend: {
+                data:['户数','户平均面积','间平均面积']
+            },
+            xAxis: [
+                {
+                    type: 'category',
+                    axisTick: {
+                        alignWithLabel: true
+                    },
+                    data: ['1949年以前','1949年-1959年','1959年-1969年','1969年-1979年','1979年-1989年','1989年-1999年','2000年以上']
+                }
+            ],
+            yAxis: [                    
+                {
+                    type: 'value',
+                    name: '户数',
+                    min: 0,
+                    position: 'left',
+                    axisLine: {
+                        lineStyle: {
+                            color: colors[0]
+                        }
+                    },
+                    axisLabel: {
+                        formatter: '{value}户'
+                    }
+                },
+                {
+                    splitLine:{show: false},
+                    type: 'value',
+                    name: '户平均面积',
+                    min: 0,
+                    position: 'right',
+                    offset: 80,
+                    axisLine: {
+                        lineStyle: {
+                            color: colors[1]
+                        }
+                    },
+                    axisLabel: {
+                        formatter: '{value}平方米'
+                    }
+                },
+                {
+                    splitLine:{show: false},
+                    type: 'value',
+                    name: '间平均面积',
+                    min: 0,
+                    position: 'right',
+                    axisLine: {
+                        lineStyle: {
+                            color: colors[2]
+                        }
+                    },
+                    axisLabel: {
+                        formatter: '{value}平方米'
+                    }
+                }
+            ],
+            series: [
+                {
+                    barWidth:40,
+                    name:'户数',
+                    type:'bar',
+                    data: hushu
+                    
+                },
+                {
+                    name:'户平均面积',
+                    type:'line',
+                    yAxisIndex: 1,
+                    data:  humianji
+                   
+                },
+                {
+                    name:'间平均面积',
+                    type:'line',
+                    yAxisIndex: 2,
+                    
+                    data: jianmianji
+                }
+            ]
+        };
+
+        house2Chart.setOption(option);
+
+    }); 
+}
+house2();
